8a77f34eadaca8c202582659d946fc448899dc20
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-10-09 23:26:14 +0300

    upd

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="grid-container bg-[#111111] mt-auto py-[18px] pb-12 Neue z-[1]">
+    <!-- <footer class="grid-container bg-[#111111] mt-auto py-[18px] pb-12 Neue z-[1]">
         <div class="flex flex-col items-center gap-6">
             <div class="flex gap-3 pb-[22px]">
                 <NuxtLink to="#" class="flex justify-center items-center xl:h-8 xl:w-8 w-12 h-12 rounded-[12px] bg-[rgba(128,128,128,.2)]">
@@ -33,6 +33,44 @@
                 <img src="/images/footer/16.png" alt="">
             </NuxtLink>
         </div>
+    </footer> -->
+
+    <footer class="grid-container pt-16 md:pt-20 xl:pt-40">
+        <div class="flex flex-col items-center gap-8 md:gap-12 xl:gap-[56px]">
+            <p class="text-2xl md:text-[40px] xl:text-[55px]">CONTACTS</p>
+            <div class="flex items-center gap-9 xl:gap-16">
+                <NuxtLink to="/" class="group relative hover:scale-125 hover:outline-[#2EECC5]/10 transition-all duration-500 rounded-full flex items-center justify-center w-10 h-10 md:w-12 md:h-12 xl:w-20 xl:h-20 bg-white outline outline-1 outline-white/10 outline-offset-[10px]">
+                    <Icon name="ic:baseline-telegram" class="text-2xl md:text-3xl xl:text-4xl text-black z-[2]"/>
+                    <span class="absolute w-full h-full z-[1] radial top-0 left-0 opacity-0 group-hover:opacity-100 rounded-full transition-all duration-500"></span>
+                </NuxtLink>
+                <NuxtLink to="/" class="group relative hover:scale-125 hover:outline-[#2EECC5]/10 transition-all duration-500 rounded-full flex items-center justify-center w-10 h-10 md:w-12 md:h-12 xl:w-20 xl:h-20 bg-white outline outline-1 outline-white/10 outline-offset-[10px]">
+                    <Icon name="ic:baseline-discord" class="text-2xl md:text-3xl xl:text-4xl text-black z-[2]"/>
+                    <span class="absolute w-full h-full z-[1] radial top-0 left-0 opacity-0 group-hover:opacity-100 rounded-full transition-all duration-500"></span>
+                </NuxtLink>
+                <NuxtLink to="/" class="group relative hover:scale-125  hover:outline-[#2EECC5]/10 transition-all duration-500 rounded-full flex items-center justify-center w-10 h-10 md:w-12 md:h-12 xl:w-20 xl:h-20 bg-white outline outline-1 outline-white/10 outline-offset-[10px]">
+                    <Icon name="mdi:twitch" class="text-2xl md:text-3xl xl:text-4xl text-black z-[2]"/>
+                    <span class="absolute w-full h-full z-[1] radial top-0 left-0 opacity-0 group-hover:opacity-100 rounded-full transition-all duration-500"></span>
+                </NuxtLink>
+            </div>
+            <div class="flex items-center gap-9 mt-4">
+                <NuxtLink to="/">                    
+                    <img src="/images/footer/valorant.png" alt="" class="w-[35px] xl:w-[50px]">
+                </NuxtLink>
+                <NuxtLink to="/">                    
+                    <img src="/images/footer/riot.png" alt="" class="w-[70px] xl:w-[100px]">
+                </NuxtLink>
+            </div>
+            <p class="max-w-[620px] text-center font-normal text-base text-white/50">
+                © 2023 Riot Games, Inc. Riot Games, VALORANT, and any associated logos are trademarks, service marks, and/or registered trademarks of Riot Games, Inc.
+            </p>
+        </div>
+
     </footer>
 </template>
+
+<style>
+    .radial{
+        background: radial-gradient(77.86% 77.86% at 49.29% 50.71%, #2EECC5 0%, #00644F 100%);
+    }
+</style>
 
--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
     <div class="flex flex-col gap-16 md:gap-20 xl:gap-36 pt-20 md:pt-36 xl:pt-56">
         <div class="flex lg:items-center justify-between max-lg:flex-col gap-8 lg:gap-10">
             <div class="flex flex-col gap-4 w-full lg:max-w-[55%]">
-                <p class="main_title text-[24px] md:text-[40px] xl:text-[55px] uppercase">Об игре</p>
+                <p class="main_title text-2xl md:text-[40px] xl:text-[55px] uppercase">Об игре</p>
                 <p class="text-sm xl:text-base text-white/60">Сочетайте свой стиль и опыт на глобальной конкурентной арене. У вас есть 13 раундов, чтобы атаковать и защищать свою сторону, используя острую стрельбу и тактические способности. И, имея по одной жизни за раунд, вам нужно будет думать быстрее, чем ваш противник, если вы хотите выжить. Сразитесь с врагами в соревновательном и нерейтинговом режимах, а также в Deathmatch и Spike Rush.</p>
             </div>
             <div class="w-full lg:max-w-[45%] bg-black h-[335px]"></div>
@@ -35,6 +35,7 @@
             </div>
         </div>
     </div>
+    
 </template>
 
 <script setup>
