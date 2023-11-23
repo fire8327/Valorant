704fd8cef9fe42caf5c40398f9f83009e574b960
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-11-23 23:27:58 +0300

    upd

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,5 @@
 <template>
+    <MainScreen/>
     <Header/>
     <main class="grid-container z-[1]">
         <NuxtPage/>
--- pages/maps/index.vue
+++ pages/maps/index.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="grid grid-cols-4 gap-8">
+    <div class="w-full px-4 md:px-6 xl:px-8 py-2 bg-gradient-to-r from-[#303134] to-transparent rounded-lg">
+        <p class="text-2xl md:text-3xl xl:text-4xl uppercase main_title">Карты</p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 mt-6 xl:mt-8">
         <NuxtLink to="/" class="flex flex-col gap-4 h-full border border-white/20 transition-all duration-300 relative overflow-hidden hover:scale-110 hover:shadow-[0px_0px_38px_0px_rgba(46,236,197,0.20)] items-center text-center rounded-xl" v-for="map in maps">
-            <img loading="lazy" :src="`/images/maps/${map.catalogImg}.png`" alt="">
+            <img loading="lazy" :src="`/images/maps/${map.catalogImg}.png`" alt="" class="w-full">
             <p class="uppercase text-lg font-bold text-[#B5FEEF]">{{ map.title }}</p>
             <p class="text-white/90 leading-[123.1%] font-normal px-2 grow">{{ map.desc.substring(0, 150) }}...</p>
             <div class="w-full left-0 bottom-0 h-2 bg-[#2EECC5]"></div>
